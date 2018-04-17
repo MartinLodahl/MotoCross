dcc385313a2d12a3dc8de0f31b4f17b4cc1268ec
MartinLodahl <cphma368@cphbusiness.dk>
2018-04-17 19:35:52 +0200

    Start off project

---
+++ Assets/OnebuttonGame/scripts/PlayButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour
+{
+    public enum Page
+    { // your enum type for syate gui
+        Restart
+    }
+    private Page current;
+    void Start()
+    {
+       
+    }
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 70, 50, 30), "Reset"))
+            Application.LoadLevel(Application.loadedLevel);
+
+    }
+}
---
+++ Assets/OnebuttonGame/scripts/onebutton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onebutton : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton("Fire1"))
+        {
+            rb.velocity = -transform.right* speed;
+        }
+    }
+}
