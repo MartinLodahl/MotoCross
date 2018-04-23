08dd286c659ac2014504ae7adb4eeb134f98d976
MartinLodahl <cphma368@cphbusiness.dk>
2018-04-23 15:04:16 +0200

    Ali angriber nu

---
+++ Ali angriber!/Assets/Scripts/detect.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detect : MonoBehaviour {
+
+    public string whattotrigger;
+
+    private bool attacking = false;
+    private int tal = 0;
+
+    public movement movementscript;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        tal++;
+        if (tal == 1){
+            this.gameObject.tag = "NoneCollidable";
+        }
+        if (attacking)
+        {
+
+        }
+        Debug.Log("NO TRIGGER" + whattotrigger);
+        if(other.tag == whattotrigger)
+        {
+            attacking = true;
+            movementscript.stopMoving(); 
+            //Do stuff
+            Transform temp_transform = other.gameObject.transform;
+            Debug.Log("HIT!");
+        }
+        
+        
+    }
+}
---
+++ Ali angriber!/Assets/Scripts/movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public Transform movetowards;
+    public float speed;
+    private bool moving = true;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, movetowards.position, step);
+        }
+    }
+
+    public void stopMoving()
+    {
+        moving = false;
+    }
+}
